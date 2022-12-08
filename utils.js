55b799b1998e3381324411a9398d0c4ca90eb640
Edouard Misset <edouardmisset@gmail.com>
2022-12-08 17:31:52 +0100

    initial setup

---
+++ utils.js
@@ -0,0 +1,11 @@
+import * as fs from 'fs/promises'
+
+export const readTxtFile = async (pathToFile) => {
+  try {
+    return await fs.readFile(pathToFile, {
+      encoding: 'utf-8'
+    })
+  } catch (message) {
+    return console.error(message)
+  }
+}
